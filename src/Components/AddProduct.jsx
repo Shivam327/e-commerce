5a173bf3f585974574cb2e7477cc3931da0b4d17
Shivam <shivamthaker1999@gmail.com>
2022-01-17 01:06:02 +0530

    Admin Login & Add Product From Added

---
+++ src/Components/AddProduct.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+
+const AddProduct = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [xx, setxx] = useState('');
+  const categories = [
+    'Accessory',
+    'Book',
+    'Computer',
+    'Fashion',
+    'Electronic',
+    'Mobile',
+  ];
+  const addProduct = (e) => {
+    e.preventDefault();
+    alert(name, description, xx);
+  };
+  return (
+    <>
+      <div className='col-md'></div>
+
+      <div className='col-md-4  offset-md-4 border  border-3 border-primary  my-5 text-primary'>
+        <form onSubmit={(e) => addProduct(e)}>
+          <div className='row g-3 align-items-center m-3'>
+            <div className='col-auto'>
+              <label className='col-form-label'>Category</label>
+            </div>
+            <div className='col-auto'>
+              <select
+                class='form-select form-select-sm col-auto'
+                aria-label='.form-select-sm example'
+              >
+                {categories.map((category) => (
+                  <option value={category}> {category} </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className='row g-3 align-items-center m-3'>
+            <div className='col-auto'>
+              <label className='col-form-label'>Username</label>
+            </div>
+            <div className='col-auto'>
+              <input
+                type='text'
+                value={name}
+                className='form-control'
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className='row g-3 align-items-center m-3'>
+            <div className='col-auto'>
+              <label className='col-form-label'>Description</label>
+            </div>
+            <div className='col-auto'>
+              <input
+                type='text'
+                value={description}
+                className='form-control'
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className='row g-3 align-items-center m-3'>
+            <div className='col-auto'>
+              <label className='col-form-label'>xx</label>
+            </div>
+            <div className='col-auto'>
+              <input
+                type='text'
+                value={xx}
+                className='form-control'
+                onChange={(e) => setxx(e.target.value)}
+              />
+            </div>
+          </div>
+          <button type='submit' className='btn btn-primary m-3 offset-5'>
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className='col-md'></div>
+    </>
+  );
+};
+
+export default AddProduct;
