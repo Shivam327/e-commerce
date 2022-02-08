d3cc82c75b2857df1889347ae0b22b0e3575e990
Shivam <shivamthaker1999@gmail.com>
2022-02-08 15:26:54 +0530

    Admin panel Added Data Adding & Fetching Added

--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -1,38 +1,38 @@
-import React, { useContext } from 'react';
-import AuthContext from '../store/store';
+import React, { useState } from 'react';
+import Modal from './EditModal';
+import { success } from '../toastify';
 
-const ItemCard = (props) => {
-  const { isLoggedIn } = useContext(AuthContext);
+const ItemCard = ({ id, price, name, description, isEditable }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen({ edit: false, view: false });
+  };
+
   return (
     <div className='col-sm-6 my-3'>
       <div className='row m-1 border border-primary'>
-        <div className='col-3 border border-danger'>
-          <img src={props.src} class='card-img-top' alt={props.name} />
-        </div>
+        <div className='col-3 border border-danger'>{/* <img src={src} className='card-img-top' alt={name} /> */}</div>
         <div className='col'>
-          <div class='card w-100'>
-            <div class='card-body justify-content-md-end'>
-              <h5 class='card-title'>{props.name}</h5>
-              <p class='card-text'>{props.desc}</p>
-              <button
-                type='submit'
-                class='btn btn-primary'
-                onClick={(props) => {
-                  alert(props.id);
-                }}
-              >
-                Add to Cart
-              </button>
-              {isLoggedIn && (
-                <button type='button' class='btn btn-primary ml-1'>
-                  {' '}
+          <div className='card w-100'>
+            <div className='card-body justify-content-md-end'>
+              <h5 className='card-title'>{name}</h5>
+              <p className='card-text'>{description}</p>
+              <p className='card-text'>$ {price}</p>
+              {isEditable ? (
+                <button type='button' className='btn btn-primary ml-1' onClick={() => setOpen(!open)}>
                   Edit
+                </button>
+              ) : (
+                <button type='submit' className='btn btn-primary' onClick={() => success('Cart')}>
+                  Add to Cart
                 </button>
               )}
             </div>
           </div>
         </div>
       </div>
+      {open && <Modal id={id} name={name} description={description} isEditable={false} onclose={onclose} />}
     </div>
   );
 };
